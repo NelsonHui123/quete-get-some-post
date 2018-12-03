5b73fc7cd578c1383dd407f66ae01ba335044183
Nelson Hui <nelson.hui123@gmail.com>
2018-12-03 13:10:12 +0100

    Quete Can I get somme POST ?

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
 // Code de la quête 1 - Récupération paramètre d'URL
 router.get('/article-:numeroArticle(\\d+)', (req, res) => {
   console.log(req.params.numeroArticle); // retourne le 3
-  res.json({ resultat: `Vous avez demandé l'article ${req.params.numeroArticle}` }); 
+  res.json({ resultat: `Vous avez demandé l'article ${req.params.numeroArticle}` });
 });
 
 // Code de la quête 2 - Récupération du GET
@@ -17,6 +16,19 @@ router.get('/mon-url', (req, res) => {
 router.post('/mon-url', (req, res) => {
   console.log(req.body.username); // retourne Bob
   res.json(req.body);
+});
+
+router.get('/forms-:codeForms(\\d+)', (req, res) => {
+  console.log('Code : ', req.params.codeForms);
+  console.log('Level : ', req.query.level);
+  res.json({ resultat: `Code : ${req.params.codeForms}, level : ${req.query.level}` });
+});
+
+router.post('/forms-:codeForms(\\d+)', (req, res) => {
+  console.log("Code : ",req.params.codeForms);
+  console.log('Level : ', req.query.level);
+  console.log('Username : ', req.body.username);
+  res.json({ resultat: `Code : ${req.params.codeForms}, Level : ${req.query.level}, Body-username: ${req.body.username}` });
 });
 
 module.exports = router;
